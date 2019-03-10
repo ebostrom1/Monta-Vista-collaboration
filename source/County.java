70505f02818fe273079e884eea1f5b78c0b68574
Eric Bostrom <eric@porcas.com>
2019-03-10 15:36:13 -0700

    3/11 due date all parsed

--- source/County.java
+++ source/County.java
@@ -1,10 +1,18 @@
+import java.util.ArrayList;
+
 public class County {
     private int FPSnum;
     private String name, State;
+    private EducationData edData;
+    private UnemplymentData jobData;
+    private ElectionResults electData;
 
-    public County(int FPSnum, String name, String state) {
+    public County(int FPSnum, String name, String state, EducationData edData, UnemplymentData jobData, ElectionResults electData) {
         this.FPSnum = FPSnum;
         this.name = name;
         State = state;
+        this.edData = edData;
+        this.jobData = jobData;
+        this.electData = electData;
     }
 }
