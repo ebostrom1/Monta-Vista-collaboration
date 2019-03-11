6ab429310bdf261746f7307004e59189ef80e79d
Eric Bostrom <eric@porcas.com>
2019-03-10 21:36:40 -0700

    3/11 due date all parsed

---
+++ source/State.java
@@ -0,0 +1,9 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class State {
+private HashMap<String, County> counties;
+    public State(HashMap<String, County> counties)  {
+        this.counties =counties;
+    }
+}
