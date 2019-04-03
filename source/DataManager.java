3568a4067fb42794d880217867dbf1816e372b11
Eric Bostrom <eric@porcas.com>
2019-04-02 18:15:45 -0700

    all done

--- source/DataManager.java
+++ source/DataManager.java
@@ -10,34 +10,44 @@ public class DataManager {
         CSVData = new HashMap<>();
     }
     public void addUnemployment(ArrayList<Unemployment> unemploymentData){
+        System.out.println("Unemployment before " + CSVData);
         for(Unemployment unemployment:unemploymentData) {
             if (!CSVData.containsKey(unemployment.getTime())){
                 CSVData.put(unemployment.getTime(), new CSVLine(unemployment.getTime()));
             }
             CSVData.get(unemployment.getTime()).addUnemployment(unemployment.getUnemploymentRate());
         }
+        System.out.println("Unemployment After " + CSVData.size());
     }
 
     public void addCrime(ArrayList<Time> crimeRecord){
+        System.out.println("Crime before " + CSVData.size());
         for(Time t: crimeRecord){
             if(!CSVData.containsKey(t)){
                 CSVData.put(t, new CSVLine(t));
             }
-            System.out.println(CSVData.get(t));
             CSVData.get(t).addCrime();
         }
+        System.out.println("Crime after " + CSVData.size());
     }
     public void setEducationData(int year, double numberOfStudentsWithHighSchoolEducation){
+        System.out.println("Education before " + CSVData.size());
         for(Map.Entry<Time ,CSVLine> entry: CSVData.entrySet()){
             if (entry.getKey().getYear() == year)
                 CSVData.get(entry.getKey()).setEducation(numberOfStudentsWithHighSchoolEducation);
         }
+        System.out.println("Education After " + CSVData.size());
     }
 
     public String printSortCSV(){
+        System.out.println("print size " + CSVData.size());
+        int size = CSVData.size();
         StringBuilder output = new StringBuilder();
-        for (int i = 0; i < CSVData.size(); i++) {
+        for (int i = 0; i < size; i++) {
             Time smallestTime = new Time();
+            System.out.println(CSVData.size());
+            System.out.println(i);
+
             for (Map.Entry<Time, CSVLine> line : CSVData.entrySet()) {
                 if (line.getValue().getTime().comparableTime()<smallestTime.comparableTime()){
                     smallestTime = line.getValue().getTime();
--- source/main.java
+++ source/main.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.HashMap;
 
 public class main {
     public static void main(String[] args) {
